d18811e08a5bafc235d563ced133a9d5bc4d1c20
AMMing <y2443@163.com>
2016-03-04 00:13:39 +0800

    rename

--- codes/SeaColor/js/seacolor.js
+++ codes/SeaColor/js/seacolor.js
@@ -136,11 +136,72 @@ var AMing;
         Core.Helper = Helper;
     })(Core = AMing.Core || (AMing.Core = {}));
 })(AMing || (AMing = {}));
-(function () {
-    var path = '/template/SeaColor';
-    var debug_path = 'http://ts.amoe.me/seacolor';
-    var now_url = window.location.href.toLowerCase();
-    var is_debug = now_url.indexOf('debug=ture') > 0; //手动进入调试页面
-    AMing.Core.Helper.append_js((is_debug ? debug_path : path) + "/js/append.js", function () { return (new SeaColor.Append(path)).init(); });
-})();
+var SeaColor;
+(function (SeaColor) {
+    /**
+     * 追加引用
+     */
+    var Append = (function () {
+        function Append() {
+            this.is_debug = true;
+            /**
+             * 通用的js目录路径
+             */
+            this.path = '/template/SeaColor/';
+            this.debug_path = 'http://ts.amoe.me/seacolor/';
+        }
+        Append.prototype.getFileUrl = function (name) {
+            var url = "" + this.path + name;
+            if (this.is_debug) {
+                url = "" + this.debug_path + name + "?v=" + (new Date()).getUTCMilliseconds();
+            }
+            return url;
+        };
+        /**
+         * 引用js文件
+         * @param name 文件名
+         */
+        Append.prototype.Js = function (name) {
+            AMing.Core.Helper.append_js(this.getFileUrl("js/" + name + ".js"));
+        };
+        /**
+         * 引用min.js文件
+         * @param name 文件名
+         */
+        Append.prototype.JsMini = function (name) {
+            this.Js("" + name + (this.is_debug ? '' : '.min'));
+        };
+        /**
+         * 引用css文件
+         * @param name 文件名
+         */
+        Append.prototype.Css = function (name) {
+            AMing.Core.Helper.append_css(this.getFileUrl("css/" + name + ".css"));
+        };
+        /**
+         * 引用min.css文件
+         * @param name 文件名
+         */
+        Append.prototype.CssMini = function (name) {
+            this.Css("" + name + (this.is_debug ? '' : '.min'));
+        };
+        /**
+         * 初始化
+         */
+        Append.prototype.init = function () {
+            var now_url = window.location.href.toLowerCase();
+            this.is_debug = now_url.indexOf('debug=ture') > 0; //手动进入调试页面
+            this.CssMini('seacolor');
+            if (jQuery('#f_pst,#postbox').length > 0) {
+                this.JsMini('kaomoji');
+            }
+            if (now_url.indexOf('mod=medal') > 0 && now_url.indexOf('action=log') < 0) {
+                this.JsMini('medal');
+            }
+        };
+        return Append;
+    }());
+    SeaColor.Append = Append;
+})(SeaColor || (SeaColor = {}));
+(new SeaColor.Append()).init();
 //# sourceMappingURL=seacolor.js.map
--- codes/SeaColor/ts/seacolor.ts
+++ codes/SeaColor/ts/seacolor.ts
@@ -118,10 +118,67 @@ module AMing.Core {
         }
     }
 }
-(() => {
-    let path = '/template/SeaColor';
-    let debug_path = 'http://ts.amoe.me/seacolor';
-    let now_url = window.location.href.toLowerCase();
-    let is_debug = now_url.indexOf('debug=ture') > 0;//手动进入调试页面
-    AMing.Core.Helper.append_js(`${is_debug ? debug_path : path}/js/append.js`, () => (new SeaColor.Append(path)).init());
-})();
+module SeaColor {
+    /**
+     * 追加引用
+     */
+    export class Append {
+        is_debug: boolean = true;
+        /**
+         * 通用的js目录路径
+         */
+        path: string = '/template/SeaColor/';
+        debug_path: string = 'http://ts.amoe.me/seacolor/';
+        getFileUrl(name: string): string {
+            let url = `${this.path}${name}`;
+            if (this.is_debug) {
+                url = `${this.debug_path}${name}?v=${(new Date()).getUTCMilliseconds()}`;
+            }
+
+            return url;
+        }
+        /**
+         * 引用js文件
+         * @param name 文件名
+         */
+        Js(name: string): void {
+            AMing.Core.Helper.append_js(this.getFileUrl(`js/${name}.js`));
+        }
+        /**
+         * 引用min.js文件
+         * @param name 文件名
+         */
+        JsMini(name: string): void {
+            this.Js(`${name}${this.is_debug ? '' : '.min'}`);
+        }
+        /**
+         * 引用css文件
+         * @param name 文件名
+         */
+        Css(name: string): void {
+            AMing.Core.Helper.append_css(this.getFileUrl(`css/${name}.css`));
+        }
+        /**
+         * 引用min.css文件
+         * @param name 文件名
+         */
+        CssMini(name: string): void {
+            this.Css(`${name}${this.is_debug ? '' : '.min'}`);
+        }
+        /**
+         * 初始化
+         */
+        init(): void {
+            let now_url = window.location.href.toLowerCase();
+            this.is_debug = now_url.indexOf('debug=ture') > 0;//手动进入调试页面
+            this.CssMini('seacolor');
+            if (jQuery('#f_pst,#postbox').length > 0) {//包含输入框才加载颜文字js
+                this.JsMini('kaomoji');
+            }
+            if (now_url.indexOf('mod=medal') > 0 && now_url.indexOf('action=log') < 0) {//是勋章页才加载js
+                this.JsMini('medal');
+            }
+        }
+    }
+}
+(new SeaColor.Append()).init();
